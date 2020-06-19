d47cc13eb92d33f113759c378504afb0346e02f9
vishal8995 <vishal8995@gmail.com>
2020-06-20 00:32:46 +0530

    Initial commit

---
+++ src/test/java/com/vishal/appinventiv/AppinventivApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishal.appinventiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppinventivApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
