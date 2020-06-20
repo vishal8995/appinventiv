f86178874089531be6284126e9e1424c9ec72ac9
vishal8995 <vishal8995@gmail.com>
2020-06-21 04:41:21 +0530

    Update Test Modifications

--- src/test/java/com/vishal/appinventiv/AppinventivApplicationTests.java
+++ src/test/java/com/vishal/appinventiv/AppinventivApplicationTests.java
@@ -10,12 +10,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -84,7 +86,10 @@ class AppinventivApplicationTests {
         existingMovie.setTitle(movie.getTitle());
         existingMovie.setCategory(movie.getCategory());
         existingMovie.setRating(movie.getRating());
-        assertEquals(existingMovie, service.createMovie(existingMovie));
+
+        assertEquals(null, service.updateMovie(existingMovie));
+
+
     }
 
 }
